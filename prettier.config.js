ce300e6f4a4eb6f562410ee4fbdad477fdfca2f3
Max Kajiwara <maxkajiwara@gmail.com>
2025-08-16 14:30:12 -0700

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+	plugins: ['prettier-plugin-tailwindcss'],
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	printWidth: 94,
+	trailingComma: 'es5',
 };
